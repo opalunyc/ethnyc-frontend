eecf89ad2e707e85cd57757094a122d31f58cfe1
Austin Baltes <austinba@gmail.com>
2022-06-25 16:56:24 -0400

    implement metamask login workflow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import metamaskFox from './MetaMask_Fox.png';
 import { Box, Button, Card, InputAdornment, TextField, Stack, Tab, Tabs, ToggleButtonGroup, ToggleButton, Tooltip, OutlinedInput} from '@mui/material';
+import { Send } from '@mui/icons-material';
 import { TabContext, TabPanel } from '@mui/lab';
 import { CartesianGrid, XAxis, YAxis, Scatter, ScatterChart, ResponsiveContainer } from 'recharts';
 import * as R from 'ramda';
@@ -14,32 +15,60 @@ const fastDispersementStartMonth = 6;
 const mediumDispersementStartMonth = 12;
 const slowDispersementStartMonth = 18;
 
-const connectWalletHandler = async () => {
-  const { ethereum } = window;
-
-  if (!ethereum) {
-    alert("Please install Metamask!");
-  }
-
+const isWalletConnected = async () => {
   try {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
-    console.log("Found an account! Address: ", accounts[0]);
-  } catch (err) {
-    console.error(err);
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("no window.ethereum");
+      return false;
+    }
+    const accounts = await ethereum.request({ method: 'eth_accounts'});
+    console.log("accounts: ", accounts);
+
+    if (accounts.length > 0) {
+      const account = accounts[0];
+      console.log("wallet is connected!", + account);
+      return true;
+    } else {
+      console.log("make sure MetaMask is connected");
+      return false;
+    }
+  } catch (error) {
+    console.log("error: ", error)
   }
+  return false;
 }
 
-connectWalletHandler();
+const connectWallet = async () => {
+  const { ethereum } = window;
+  if (!ethereum) {
+    throw new Error("no window.ethereum");
+  }
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
+  console.log("accounts: ", accounts);
+
+  if (accounts.length > 0) {
+    const account = accounts[0];
+    console.log("wallet is connected!", + account);
+  } else {
+    throw new Error("metamask not connected");
+  }
+};
+
 
 const StatusPage = () => {
   return (<Stack spacing={2}>
     <Card className="Card">
-      <p>Your last check in was at June 25, 2022 1:31am.</p>
-      <p>You must check in again before June 24, 2023 1:31am before dispersement begins.</p>
+      <Box sx={{margin:1}}>
+        <p>Your last check in was at June 25, 2022 1:31am.</p>
+        <p>You must check in again before June 24, 2023 1:31am before dispersement begins.</p>
+      </Box>
     </Card>
     <Card className="Card">
-      <p>This is your wallet address: <strong>0x123797324001238098</strong></p>
-      <p>Wallet Balance: <strong>{ethInAccount} ethx</strong></p>
+      <Box sx={{margin:1}}>
+        <p>This is your wallet address: <strong>0x123797324001238098</strong></p>
+        <p>Wallet Balance: <strong>{ethInAccount} fdaix</strong></p>
+      </Box>
     </Card>
   </Stack>);
 };
@@ -49,13 +78,17 @@ const BeneficiaryPage = () => {
   const handleSchedule = (e, newSchedule) => setSchedule(newSchedule);
   return (<Stack spacing={2}>
     <Card className="Card">
-      <TextField
-        label="Beneficiary Address"
-        fullWidth
-      />
-      <Button variant="contained" size="large">
-        Update
-      </Button>
+      <Box sx={{ margin: 1}}>
+        <TextField
+          label="Beneficiary Address"
+          fullWidth
+        />
+      </Box>
+      <Box sx={{ margin: 1}}>
+        <Button variant="contained" size="large" disabled>
+          Update
+        </Button>
+      </Box>
     </Card>
     <DispersementSchedule value={schedule} onChange={handleSchedule} />
   </Stack>);
@@ -71,30 +104,29 @@ const DispersementSchedule = ({value, onChange}) => {
 
   return (
     <Card className="Card">
-      <div className="section-title">Dispersement Schedule</div>
-      <ToggleButtonGroup value={value} exclusive className="dispersement-speed-selector" onChange={onChange}>
-        <ToggleButton value="fast">
-          Fast Dispersement
-        </ToggleButton>
-        <ToggleButton value="medium">
-          Medium Dispersement
-        </ToggleButton>
-        <ToggleButton value="slow">
-          Slow Dispersement
-        </ToggleButton>
-      </ToggleButtonGroup>
-      <ResponsiveContainer width="100%" height={300}>
-        <ScatterChart width={600} height={300} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <XAxis dataKey="x" type="time" scale="linear" name="years" unit=" years" range={[0, dispersementData.length/12]} interval="preserveStartEnd" allowDecimals={false}/>
-          <YAxis yAxisId="qty" dataKey="y" type="number" scale="linear" name="eth" unit=" ethx" width={100} range={[0, 'dataMax']} interval="preserveStartEnd" allowDecimals={false} />
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter yAxisId="qty" name="Dispersement Schedule" data={dispersementData} fill="#8884d8" line/>
-
-          
-        </ScatterChart>
-      </ResponsiveContainer>
-      
+      <Box sx={{ margin: 1 }}>
+        <div className="section-title">Dispersement Schedule</div>
+        <ToggleButtonGroup value={value} exclusive className="dispersement-speed-selector" onChange={onChange}>
+          <ToggleButton value="fast">
+            Fast Dispersement
+          </ToggleButton>
+          <ToggleButton value="medium">
+            Medium Dispersement
+          </ToggleButton>
+          <ToggleButton value="slow">
+            Slow Dispersement
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <ResponsiveContainer width="100%" height={300}>
+          <ScatterChart width={600} height={300} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="x" type="time" scale="linear" name="years" unit=" years" range={[0, dispersementData.length/12]} interval="preserveStartEnd" allowDecimals={false}/>
+            <YAxis yAxisId="qty" dataKey="y" type="number" scale="linear" name="eth" unit=" fdaix" width={100} range={[0, 'dataMax']} interval="preserveStartEnd" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter yAxisId="qty" name="Dispersement Schedule" data={dispersementData} fill="#8884d8" line/>
+          </ScatterChart>
+        </ResponsiveContainer>
+      </Box>
     </Card>
   );
 };
@@ -102,15 +134,21 @@ const DispersementSchedule = ({value, onChange}) => {
 const WithdrawPage = () => {
   return (<Stack spacing={2}>
     <Card className="Card">
-      <div className="section-title">Withdraw</div>
-      <TextField
-        label="Recipient Wallet Address"
-        fullWidth
-      />
-      <Box><OutlinedInput label="Quantity" type="number" endAdornment={<InputAdornment position="end">ethx</InputAdornment>} /></Box>
-      <Button variant="contained" size="large">
-        Send
-      </Button>
+      <Box sx={{margin: 1}} className="section-title">Withdraw</Box>
+      <Box sx={{margin: 1}}>
+        <TextField
+          label="Recipient Wallet Address"
+          fullWidth
+        />
+      </Box>
+      <Box sx={{margin: 1}}>
+        <OutlinedInput label="Quantity" type="number" notched={false} endAdornment={<InputAdornment position="end">fdaix</InputAdornment>} />
+        </Box>
+      <Box sx={{margin: 1}}>
+        <Button variant="contained" size="large" endIcon={<Send />} disabled>
+          Send
+        </Button>
+      </Box>
     </Card>
   </Stack>);
 }
@@ -138,31 +176,59 @@ const Dashboard = () => {
   );
 };
 
-const LoginToMetamask = () => {
+const LoginToMetamask = ({setLoggedInState}) => {
   return (<Box className="login-main-area">
     <Box sx={{ margin: 1 }} className="login-header">Login to Dribble Dapp</Box>
     <Box sx={{ margin: 1 }}>
       Connect your Metamask wallet to use Dribble Dapp
     </Box>
-    <Button className="login-button" sx={{backgroundColor: "#f0c07f", margin: 3 }}>
+    <Button className="login-button" sx={{backgroundColor: "#e3d4bf", margin: 3 }} onClick={() => {
+      setLoggedInState("loading");
+      connectWallet().then(() => {
+        // success!
+        setLoggedInState("true");
+      }).catch(error => {
+        console.log("failed to log in", error);
+        setLoggedInState("false");
+      });
+    }}>
       <img src={metamaskFox} height="100" width="100" />
       <Box sx={{ fontSize: 20}}>Login to Metamask</Box>
     </Button>
   </Box>);
 };
 
-const App = () => {
-  const [tabValue, setTabValue] = useState("0");
-  const handleTabChange = (_, newValue) => setTabValue(newValue);
-  const tab = "0";
-  return (
-    <div className="App">
-      <header>
-        Dribble Dapp
-      </header>
-      {/* <LoginToMetamask /> */}
-      <Dashboard />
-    </div>
-  );
+const CheckingIfLoggedIn = () => {
+  return (<Box>
+    Loading...
+    </Box>);
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: "loading" };
+  }
+  componentDidMount() {
+    isWalletConnected()
+      .then(status => this.setState({ loggedIn: status ? "true" : "false" }));
+  }
+  render() {
+    const setLoggedInState = (state) => this.setState({ loggedIn: state });
+    return (
+      <div className="App">
+        <header>
+          Dribble Dapp
+        </header>
+        { this.state.loggedIn === "true" ? 
+            <Dashboard /> :
+            ( this.state.loggedIn === "false" ?
+                <LoginToMetamask setLoggedInState={setLoggedInState} /> :
+                <CheckingIfLoggedIn /> )
+        }
+        
+      </div>
+    );
+  }
 };
 export default App;
